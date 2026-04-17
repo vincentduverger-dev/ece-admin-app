3dba3ef4a509def44d0b966f5f8c221b22d25db2
Vincent Duverger <vincent.duverger.dev@gmail.com>
2026-04-17 10:36:41 +0200

    feat(api): add GET /api/applications/:id endpoint

--- apps/api/src/routes/application.routes.ts
+++ apps/api/src/routes/application.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
-import { getApplications } from "../controllers/application.controller";
+import {
+  getApplicationById,
+  getApplications
+} from "../controllers/application.controller";
 
 const router = Router();
 
 router.get("/applications", getApplications);
+router.get("/applications/:id", getApplicationById);
 
 export default router;
